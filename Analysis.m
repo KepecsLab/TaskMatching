c53858a228fdc48c135f496e04c62e8822105642
Suelynn <sren1340@gmail.com>
2020-09-02 18:18:24 -0500

    Delete Analysis.m
    temp analysis file accidentally pushed

--- Analysis.m
+++
@@ -1,8 +0,0 @@
-ChoiceLeft = SessionData.Custom.ChoiceLeft;
-LeftHi = SessionData.Custom.LeftHi;
-Subject = SessionData.Custom.Subject;
-%%
-figure;hold on;
-plot(smooth(ChoiceLeft,10,'moving'));
-plot(LeftHi);
-title(Subject)
